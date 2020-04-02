8e676ebc113b30c0b92dba455ccb62e679e4a5e4
Ha Trong Nghia <hatrongnghia@outlook.com>
2020-04-02 21:15:54 +0700

    Third commit

--- sorting-algorithm/bubble_sort.py
+++ sorting-algorithm/bubble_sort.py
@@ -1,5 +1,3 @@
-import time
-start_time = time.time()
 l = [5, 8, 1, 3, 2]
 swapping = True
 while swapping:
@@ -9,7 +7,3 @@ while swapping:
             l[i], l[i+1] = l[i+1], l[i]
             swapping = True
 print(l)
-end_time = time.time()
-
-total_time = end_time - start_time
-print(total_time)
