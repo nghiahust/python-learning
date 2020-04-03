6f1d0749bc6a349b7c09f4b7d8a0d56f36466e85
Ha Trong Nghia <hatrongnghia@outlook.com>
2020-04-03 18:09:46 +0700

    Linh tinh lung tung

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,27 @@
 def fibonacci_iterative(n):
-    if n == 0 or n == 1:
+    previous = 0
+    current = 1
+    if n <= 1:
         return n
     else:
-        return fibonacci_iterative(n - 1) + fibonacci_iterative(n - 2)
+        for i in range(2, n+1):
+            current, previous = current + previous, current
+        return fibo[n]
+
+
+def fibonacci_recursive(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+def fibonacci_dynamic(n):
+    fibo = {}
+    fibo[0] = 0
+    fibo[1] = 1
+    for i in reversed(range(n + 1)):
+        if i in fibo:
+            return fibo[i]
+        else:
+            fibo[i] = fibo[i - 1] + fibo[i - 2]
+            return fibo[i]
--- testcpp.cpp
+++ testcpp.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
-
+ 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+	cout << "Hello World" << endl;
+	return 0;
 }
